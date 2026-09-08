e348483a3e9a728b9b544b2a6da8ad55c1a8fffb
Nurlan Purkhanov <ppurkhanov@gmail.com>
2026-09-08 14:25:59 +0500

    Add redactIP function to mask client IP addresses in logs for enhanced privacy

--- server.go
+++ server.go
@@ -129,6 +129,24 @@ func httpError(ctx context.Context, w http.ResponseWriter, status int, err error
 	http.Error(w, msg, status)
 }
 
+func redactIP(addr string) string {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		host = addr
+	}
+
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return host
+	}
+
+	if ip4 := ip.To4(); ip4 != nil {
+		return fmt.Sprintf("%d.%d.%d.x", ip4[0], ip4[1], ip4[2])
+	}
+
+	return ip.String()
+}
+
 func requestID() func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -159,7 +177,7 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 			attrs := []any{
 				slog.String("method", r.Method),
 				slog.String("path", r.URL.Path),
-				slog.String("client_ip", r.RemoteAddr),
+				slog.String("client_ip", redactIP(r.RemoteAddr)),
 				slog.Duration("duration", time.Since(start)),
 				slog.Int("request_body_bytes", spyReader.bytesRead),
 				slog.Int("response_status", spyWriter.statusCode),
